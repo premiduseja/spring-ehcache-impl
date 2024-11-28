f6770fd7bd60c354d197248d222e78d589bad373
Premi Duseja <58366224+premiduseja@users.noreply.github.com>
2024-11-27 19:04:02 -0500

    Initial commit

---
+++ src/main/java/com/codeforfun/ehcache/spring_ehcache_impl/SpringEhcacheImplApplication.java
@@ -0,0 +1,13 @@
+package com.codeforfun.ehcache.spring_ehcache_impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEhcacheImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEhcacheImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeforfun/ehcache/spring_ehcache_impl/SpringEhcacheImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeforfun.ehcache.spring_ehcache_impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEhcacheImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
